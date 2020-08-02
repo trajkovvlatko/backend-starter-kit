cceeb20ec2ec9d11b90455a0eba1096f0d32aa7f
Vlatko Trajkov <trajkovvlatko@gmail.com>
2020-08-02 19:12:58 +0200

    Migrations on prod

--- src/app/config/migrate.ts
+++ src/app/config/migrate.ts
@@ -1,13 +1,22 @@
 import {host, port, user, password, database} from './get';
 import {exec} from 'child_process';
 const direction = process.argv[2];
+const env = process.env.NODE_ENV || 'dev';
 
 if (['up', 'down'].indexOf(direction) === -1) {
   console.log("Invalid command, must be npm run migrate 'up/down'");
 } else {
+  let tsNode = '';
+  let tsConf = '';
+  let dir = './dist/migrations';
+  if (env === 'dev') {
+    tsNode = 'ts-node';
+    tsConf = '-j ts';
+    dir = './src/migrations';
+  }
   const dbUrl = `postgres://${user}:${password}@${host}:${port}/${database}`;
-  const migratePath = `ts-node ./node_modules/node-pg-migrate/bin/node-pg-migrate`;
-  const config = '-j ts --migrations-dir ./src/migrations';
+  const migratePath = `${tsNode} ./node_modules/node-pg-migrate/bin/node-pg-migrate`;
+  const config = `${tsConf} --migrations-dir ${dir}`;
   const command = `DATABASE_URL=${dbUrl} ${migratePath} ${config} ${direction}`;
   exec(command, (err, stdout) => {
     if (err) {
